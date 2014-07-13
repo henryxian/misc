6df4a3e68b5532d9f25cde4ec39067083fe27bb1
henryxian <henryxian@qq.com>
2014-07-14 01:31:13 +0800

    hello world

---
+++ helloworld/helloworld.cpp
@@ -0,0 +1,15 @@
+// helloworld.cpp : 定义控制台应用程序的入口点。
+// 直接创建空项目会报错，十分奇怪。VS菜鸟伤不起。
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "hello world!" << endl;
+	getchar();
+	return 0;
+}
+
