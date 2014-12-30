d7837af860bd393b81d78cf970cd6e63feeb4ab1
henryxian <henryxian@qq.com>
2014-12-30 14:04:47 +0800

    add christmas tree

--- test/源.cpp
+++ test/源.cpp
@@ -1,10 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // 链接的时候会报打不开gdi32.lib的错误，每次都要手动在属性中移除才能成功运行。
-int main() {
-	int a;
-	scanf_s("%d", &a);
-	printf("hello %d", a);
+int main(int argc, char* argv[]) {
+	//int a;
+	//scanf_s("%d", &a);
+	//printf("hello %d", a);
+	//getchar();
+	//return 0;
+	int n = argc > 1 ? atoi(argv[1]) : 4;
+	for (int j = 1; j <= n; j++) {
+		int s = 1 << j, k = (1 << n) - s, x;
+		for (int y = s - j; y >= 0; y--, putchar('\n')) {
+			for (x = 0; x < y + k; x++) printf("  ");
+			for (x = 0; x + y < s; x++) printf("%c ", '!' ^ y & x);
+			for (x = 1; x + y < s; x++) printf("%c ", '!' ^ y & (s - y - x - 1));
+		}
+	}
 	getchar();
-	return 0;
 }
