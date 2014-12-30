b9a20545163640191191c22b38165ff879a79b9d
henryxian <henryxian@qq.com>
2014-12-30 14:15:24 +0800

    rename file

--- BinaryTree/btree.cpp
+++ BinaryTree/btree.cpp
@@ -1,17 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "btree.h"
+#include <iostream>
+
+using namespace std;
 
 int main() {
+	binary_tree* T2 = (binary_tree *)malloc(sizeof(binary_tree));
+	binary_tree* T3 = (binary_tree *)malloc(sizeof(binary_tree));
+	bin_tree T = (binary_tree *)malloc(sizeof(binary_tree));
+	T->left_node = T2;
+	cout << T->left_node << " " << T2 << " " << T3 << endl;
+	T2 = T3;
+	cout << T->left_node << " " << T2 << " "<< T3 << endl;
 	//printf("hello world!");
-	bin_tree T = NULL;
-	CreateBiTree(T);
+	//bin_tree T = NULL;
+	//CreateBiTree(T);
+	getchar();
 	return 0;
+
 }
 
 // 先序序列生成二叉树。
 status CreateBiTree(bin_tree &T) {
 	char ch;
+	printf("Please input a sequence:");
 	scanf_s("%c", &ch); // scanf_s()的行为有点奇怪，需要继续理解。
 	if (ch == '^') {
 		T = NULL;
--- BinaryTree/btree.h
+++ BinaryTree/btree.h
@@ -6,4 +6,19 @@ typedef struct binary_tree {
 
 typedef int status;
 
+// Create a binary tree with scanf_s
 status CreateBiTree(bin_tree &T);
+
+
+// Traverse binary tree in recursive way
+status TraverseBiTreeRecur(bin_tree T);
+
+
+// Traverse binary tree in non-recursive way
+status TraverseBiTree(bin_tree T);
+
+// Copy a binary tree.
+status CopyBiTree(bin_tree T, bin_tree &S);
+
+
+
